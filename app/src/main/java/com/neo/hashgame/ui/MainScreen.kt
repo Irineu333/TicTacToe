cfc579825d11bcfdfcdaea793e9599eea9c27fef
irineu silva <irineu.silva@aiqfome.com>
2022-07-17 01:13:09 -0300

    feat: transition animation in compose navigation using accompanist

--- app/src/main/java/com/neo/hashgame/ui/MainScreen.kt
+++ app/src/main/java/com/neo/hashgame/ui/MainScreen.kt
@@ -1,5 +1,6 @@
 package com.neo.hashgame.ui
 
+import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.Text
@@ -7,23 +8,32 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
-import androidx.navigation.compose.rememberNavController
+import com.google.accompanist.navigation.animation.AnimatedNavHost
+import com.google.accompanist.navigation.animation.composable
+import com.google.accompanist.navigation.animation.rememberAnimatedNavController
 import com.neo.hashgame.model.Score
 import com.neo.hashgame.model.Screen
 import com.neo.hashgame.ui.screen.HomeScreen
 import com.neo.hashgame.ui.theme.HashGameTheme
+import com.neo.hashgame.util.extensions.enterToLeftTransition
+import com.neo.hashgame.util.extensions.enterToRightTransition
+import com.neo.hashgame.util.extensions.exitToLeftTransition
+import com.neo.hashgame.util.extensions.exitToRightTransition
 
+@OptIn(ExperimentalAnimationApi::class)
 @Composable
 fun MainScreen() {
-    val controller = rememberNavController()
+    val controller = rememberAnimatedNavController()
 
-    NavHost(
+    AnimatedNavHost(
         navController = controller,
         startDestination = Screen.HomeScreen.route
     ) {
-        composable(Screen.HomeScreen.route) {
+        composable(
+            route = Screen.HomeScreen.route,
+            exitTransition = { exitToLeftTransition },
+            popEnterTransition = { enterToRightTransition }
+        ) {
             HomeScreen(
                 // TODO: mock
                 score = Score(
@@ -37,7 +47,11 @@ fun MainScreen() {
             )
         }
 
-        composable(Screen.GameScreen.route) {
+        composable(
+            route = Screen.GameScreen.route,
+            enterTransition = { enterToLeftTransition },
+            popExitTransition = { exitToRightTransition }
+        ) {
             Box(
                 contentAlignment = Alignment.Center,
                 modifier = Modifier.fillMaxSize()
---
+++ app/src/main/java/com/neo/hashgame/util/extensions/Transitions.kt
@@ -0,0 +1,35 @@
+package com.neo.hashgame.util.extensions
+
+import androidx.compose.animation.AnimatedContentScope
+import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.animation.core.tween
+
+const val TRANSITION_DURATION = 700
+
+@OptIn(ExperimentalAnimationApi::class)
+val AnimatedContentScope<*>.exitToLeftTransition
+    get() = slideOutOfContainer(
+        AnimatedContentScope.SlideDirection.Left,
+        animationSpec = tween(TRANSITION_DURATION)
+    )
+
+@OptIn(ExperimentalAnimationApi::class)
+val AnimatedContentScope<*>.exitToRightTransition
+    get() = slideOutOfContainer(
+        AnimatedContentScope.SlideDirection.Right,
+        animationSpec = tween(TRANSITION_DURATION)
+    )
+
+@OptIn(ExperimentalAnimationApi::class)
+val AnimatedContentScope<*>.enterToRightTransition
+    get() = slideIntoContainer(
+        AnimatedContentScope.SlideDirection.Right,
+        animationSpec = tween(TRANSITION_DURATION)
+    )
+
+@OptIn(ExperimentalAnimationApi::class)
+val AnimatedContentScope<*>.enterToLeftTransition
+    get() = slideIntoContainer(
+        AnimatedContentScope.SlideDirection.Left,
+        animationSpec = tween(TRANSITION_DURATION)
+    )
