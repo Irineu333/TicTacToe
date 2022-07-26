691a384d855437d78e65d1108afae25501980f3c
irineu silva <irineu.silva@aiqfome.com>
2022-07-26 00:58:55 -0300

    feat: create splash screen

--- app/src/main/java/com/neo/hash/MainActivity.kt
+++ app/src/main/java/com/neo/hash/MainActivity.kt
@@ -9,12 +9,16 @@ import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.neo.hash.ui.MainScreen
 import com.neo.hash.ui.theme.HashGameTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        installSplashScreen()
+
         setContent {
             HashGameTheme {
                 HashGameBackground(
