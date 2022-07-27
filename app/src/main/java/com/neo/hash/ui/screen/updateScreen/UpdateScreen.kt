a8689fc498ab229d089c7a3f33f9b86a89769ebd
irineu silva <irineu.silva@aiqfome.com>
2022-07-27 01:47:47 -0300

    chore: remove unused imports

--- app/src/main/java/com/neo/hash/ui/screen/updateScreen/UpdateScreen.kt
+++ app/src/main/java/com/neo/hash/ui/screen/updateScreen/UpdateScreen.kt
@@ -14,11 +14,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.SpanStyle
-import androidx.compose.ui.text.buildAnnotatedString
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
