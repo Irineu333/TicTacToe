f2374eb546d98802529f78fb2a3e3c3419b43ff7
Irineu333 <mmmirineusilva@gmail.com>
2022-08-24 18:08:01 -0300

    feat: improve AI functions

--- app/src/main/java/com/neo/hash/util/extensions/ListExtensions.kt
+++ app/src/main/java/com/neo/hash/util/extensions/ListExtensions.kt
@@ -1,31 +1,5 @@
 package com.neo.hash.util.extensions
 
-import androidx.compose.ui.text.AnnotatedString
-import androidx.compose.ui.text.buildAnnotatedString
-
-
-fun <T> List<T>.joinToAnnotatedString(
-    separator: String,
-    transformation: AnnotatedString.Builder.(T) -> Unit
-) = buildAnnotatedString {
-    val sequence = this@joinToAnnotatedString
-    val lastIndex = sequence.lastIndex
-
-    for ((index, item) in sequence.withIndex()) {
-        transformation(this, item)
-        if (index < lastIndex) {
-            append(separator)
-        }
-    }
-}
-
-fun <T> List<T>.containsAny(
-    emptyBlocks: MutableList<T>
-) = any { block ->
-    emptyBlocks.any {
-        block == it
-    }
-}
 
 fun <T> List<T>.recurring(
     predicate: (T) -> T = { it }
